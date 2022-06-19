0c3d42ceb0b64f12a577a541cb00747b714ed141
George J Padayatti <george.padayatti@igrant.io>
2022-06-19 16:54:07 +0530

    Setup Indy SDK and create a sample wallet
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

---
+++ src/main/kotlin/com/indy/samples/HelloWorld.kt
@@ -0,0 +1,25 @@
+package com.indy.samples
+
+import org.hyperledger.indy.sdk.LibIndy
+import org.hyperledger.indy.sdk.wallet.Wallet
+
+
+fun main() {
+
+//    Load libindy.dylib from LD_LIBRARY_PATH
+    System.loadLibrary("indy")
+
+//    Initialise libindy
+    if (!LibIndy.isInitialized()) {
+        LibIndy.init()
+    }
+
+//    Create a sample Indy wallet
+    val walletName = "SampleWallet"
+    val walletType = "default"
+    val walletConfig = "{ \"id\":\"$walletName\", \"storage_type\":\"$walletType\"}"
+    val walletCredentials =
+        "{\"key\":\"SampleWalletKey\", \"key_derivation_method\":\"RAW\"}"
+
+    Wallet.createWallet(walletConfig, walletCredentials)
+}
