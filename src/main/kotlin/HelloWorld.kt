d8a38d8f4a3aeba8f14b7c413e9b65690299e0d6
George J Padayatti <george.padayatti@igrant.io>
2022-06-19 17:39:11 +0530

    Use hashmap to create JSON configuration strings for wallet initialisation
    Signed-off-by: George J Padayatti <george.padayatti@igrant.io>

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -1,6 +1,7 @@
 import org.hyperledger.indy.sdk.LibIndy
 import org.hyperledger.indy.sdk.wallet.Wallet
-
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
 
 fun main() {
 
@@ -15,9 +16,14 @@ fun main() {
     // Create a sample Indy wallet
     val walletName = "SampleWallet"
     val walletType = "default"
-    val walletConfig = "{ \"id\":\"$walletName\", \"storage_type\":\"$walletType\"}"
-    val walletCredentials =
-        "{\"key\":\"SampleWalletKey\", \"key_derivation_method\":\"RAW\"}"
 
-    Wallet.createWallet(walletConfig, walletCredentials)
+    var walletConfig = HashMap<String, String>()
+    walletConfig.put("id", walletName)
+    walletConfig.put("storage_type", walletType)
+
+    var walletCredentials = HashMap<String, String>()
+    walletCredentials.put("key", "SampleWalletKey")
+    walletCredentials.put("key_derivation_method", "RAW")
+
+    Wallet.createWallet(Json.encodeToString(walletConfig), Json.encodeToString(walletCredentials))
 }
